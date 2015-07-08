b8588690cd17a9cc38982aaea0a5f5ce48c07395
paul.huang <paul.huang@transfinder.com>
2015-07-08 11:38:16 +0800

    继承方法例子

--- EasyRunSite/JS/homePage.js
+++ EasyRunSite/JS/homePage.js
@@ -10,3 +10,19 @@
     EZ.HomePage.prototype.constructor = HomePage;
 
 })();
+
+
+// 继承参考
+//(function()
+//{
+//	createNamespace("TF.Viewe").TripUpdate = TripUpdate;
+
+//	function TripUpdate()
+//	{
+//		TF.Viewe.BaseViewe.call(this);
+//	}
+
+//	TripViewe.prototype = Object.create(TF.Viewe.BaseViewe.prototype);
+//	TripViewe.prototype.constructor = TripViewe;
+
+//})();
