437d3f90ccc75509f2c3f9f47bd6bf90e4ffb099
paul.huang <paul.huang@transfinder.com>
2015-07-10 18:25:48 +0800

    增加商品页，联系人页，热门商品模板

--- EasyRunSite/js/basePage.js
+++ EasyRunSite/js/basePage.js
@@ -4,8 +4,8 @@
     function BasePage(startPageName) {
         this.headerTemplate = ko.observable("header");
         this.contentTemplate = ko.observable(startPageName);  //Replace by default value.
+        this.hotCommodityTemplate = ko.observable("hot_commodity");
         this.footerTemplate = ko.observable("footer");
-
         this.headerData = "";
         this.id = "";
     };
