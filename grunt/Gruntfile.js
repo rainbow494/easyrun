524ed325fd7c882045d803ff518432bfc8f125f8
rainbow494 <rainbow494@gmail.com>
2015-07-11 16:47:26 +0800

    add grunt config

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,46 +1,70 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: '<%= pkg.name %>.js',
-        dest: '<%= pkg.name %>.min.js'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		// pkg : grunt.file.readJSON('package.json'),
+		// uglify : {
+			// options : {
+				// banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			// },
+			// build : {
+				// src : '<%= pkg.name %>.js',
+				// dest : '<%= pkg.name %>.min.js'
+			// }
+		// },
+
+		less : {
+			// test : {
+				// options : {},
+				// files : {
+					// "test.css" : "test.less"
+				// }
+			// },
+			compile : {
+				// options : {
+					// //paths : ['../easyrunsite/style']
+				// },
+				files : {
+					'../easyrunsite/style/main.css' : ['../easyrunsite/style/main.less']
+					//'main.css' : ['main.less']
+				}
+			}
+		},
+
+		watch : {
+			less: {
+				//files : 'F:/GitHub/**/*.less',
+				files: ['../easyrunsite/**/*.less'],
+				tasks: ['less'],
+				options : {
+					livereload : true,
+				},
+			},
+			// css : {
+				// files : '**/*.sass',
+				// tasks : ['sass'],
+				// options : {
+					// livereload : true,
+				// },
+			// },
+		},
+	});
+
+	// // 加载包含 "uglify" 任务的插件。
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// less
+	grunt.loadNpmTasks('grunt-contrib-less');
 	
-	less: {
-	  test: {
-		options: {		  
-		},
-		files: {
-		  "test.css": "test.less"
-		}
-	  },
-	  development: {
-		options: {
-		  paths: ["assets/css"]
-		},
-		files: {
-		  "path/to/result.css": "path/to/source.less"
-		}
-	  }
-	}
-  });
+	// watch
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // 加载包含 "uglify" 任务的插件。
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-  // less
-  grunt.loadNpmTasks('grunt-contrib-less');
+	// 默认被执行的任务列表。
+	//grunt.registerTask('default', ['less', 'watch']);
+	grunt.registerTask('default', ['less','watch']);
 
-  // 默认被执行的任务列表。
-  grunt.registerTask('default', ['uglify']);
-  
-
+	grunt.registerTask('less-build', ['less:compile']);
+	
+	//grunt.registerTask('watch', ['watch']);
 
 };
