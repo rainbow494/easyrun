cc9dda366fc7d09772dbe79b378f4c2ea273525b
rainbow494 <rainbow494@gmail.com>
2015-07-11 18:16:43 +0800

    配置grunt, live reload

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,28 +1,32 @@
 module.exports = function (grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		// pkg : grunt.file.readJSON('package.json'),
-		// uglify : {
-			// options : {
-				// banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			// },
-			// build : {
-				// src : '<%= pkg.name %>.js',
-				// dest : '<%= pkg.name %>.min.js'
-			// }
-		// },
+	// LiveReload的默认端口号，你也可以改成你想要的端口号
+	var lrPort = 35729;
+	// 使用connect-livereload模块，生成一个与LiveReload脚本
+	// &lt;script src=&quot;http://127.0.0.1:35729/livereload.js?snipver=1&quot; type=&quot;text/javascript&quot;&gt;&lt;/script&gt;
+	var lrSnippet = require('connect-livereload')({
+			port : lrPort
+		});
+	// 使用 middleware(中间件)，就必须关闭 LiveReload 的浏览器插件
+	var lrMiddleware = function (connect, options) {
+		return [
+			// 把脚本，注入到静态文件中
+			lrSnippet,
+			// 静态文件服务器的路径
+			connect.static(options.base[0]),
+			// 启用目录浏览(相当于IIS中的目录浏览)
+			connect.directory(options.base[0])
+		];
+	};
 
+	// 项目配置(任务配置)
+	grunt.initConfig({
+		// 读取我们的项目配置并存储到pkg属性中
+		pkg : grunt.file.readJSON('package.json'),
 		less : {
-			// test : {
-				// options : {},
-				// files : {
-					// "test.css" : "test.less"
-				// }
-			// },
 			compile : {
 				// options : {
-					// //paths : ['../easyrunsite/style']
+				// //paths : ['../easyrunsite/style']
 				// },
 				files : {
 					'../easyrunsite/style/main.css' : ['../easyrunsite/style/main.less']
@@ -30,41 +34,58 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-
+		// 通过connect任务，创建一个静态服务器
+		connect : {
+			options : {
+				// 服务器端口号
+				port : 8000,
+				// 服务器地址(可以使用主机名localhost，也能使用IP)
+				hostname : 'localhost',
+				// 物理路径(默认为. 即根目录) 注：使用'.'或'..'为路径的时，可能会返回403 Forbidden. 此时将该值改为相对路径 如：/grunt/reloard。
+				base : '../easyrunsite/'
+			},
+			livereload : {
+				options : {
+					// 通过LiveReload脚本，让页面重新加载。
+					middleware : lrMiddleware
+				}
+			}
+		},
+		// 通过watch任务，来监听文件是否有更改
 		watch : {
-			less: {
+			client : {
+				// 我们不需要配置额外的任务，watch任务已经内建LiveReload浏览器刷新的代码片段。
+				options : {
+					livereload : lrPort
+				},
+				// '**' 表示包含所有的子目录
+				// '*' 表示包含所有的文件
+				//files: ['../easyrunsite/**/*.html', 'css/*', 'js/*', 'images/**/*']
+				files : ['../easyrunsite/**/*.*']
+			},
+			less : {
 				//files : 'F:/GitHub/**/*.less',
-				files: ['../easyrunsite/**/*.less'],
-				tasks: ['less'],
+				files : ['../easyrunsite/**/*.less'],
+				tasks : ['less'],
 				options : {
 					livereload : true,
 				},
 			},
-			// css : {
-				// files : '**/*.sass',
-				// tasks : ['sass'],
-				// options : {
-					// livereload : true,
-				// },
-			// },
-		},
-	});
+		}
+	}); // grunt.initConfig配置完毕
 
-	// // 加载包含 "uglify" 任务的插件。
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	// less
+	// 加载插件
 	grunt.loadNpmTasks('grunt-contrib-less');
-	
-	// watch
+	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// 默认被执行的任务列表。
-	//grunt.registerTask('default', ['less', 'watch']);
-	grunt.registerTask('default', ['less','watch']);
+	grunt.registerTask('default', ['less', 'connect', 'watch']);
+	// 自定义任务
+	//grunt.registerTask('less', ['less']);
 
-	grunt.registerTask('less-build', ['less:compile']);
-	
-	//grunt.registerTask('watch', ['watch']);
+	// grunt.registerTask('less-build', ['less:compile']);
+
+	// //grunt.registerTask('watch', ['watch']);
 
 };
