c88e7b745a14828c8d2c830cb94adcf5d3662c2b
rainbow494 <rainbow494@gmail.com>
2015-08-11 02:01:44 +0800

    添加nodejs后台

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,9 +1,13 @@
+
 var http = require('http');
 var url = require('url');
+
+var test = new Date();
 
 var server = http.createServer(function (req,res) {
 
     //console.log(url.parse(req.url, true));
+    console.log(2);
 
     var pathname = url.parse(req.url, true).pathname;
 
@@ -12,19 +16,18 @@ var server = http.createServer(function (req,res) {
     switch (pathname) {
         case '/api/parsetime':
         res.write(JSON.stringify({
-            "hour":  "test"
-         }));
-            break;
+            "hour":  "test1"
+        }));
+        break;
         case '/api/unixtime':
         res.write(JSON.stringify({
             "unixtime": "test2"
         }));
-            break;
+        break;
         default:
-
     }
 
     res.end();
 })
-
+console.log(2);
 server.listen(1025);
---
+++ nodejs/nodejs.md
@@ -0,0 +1,22 @@
+## 安装
+
+- 依赖模块安装
+```
+npm install
+```
+- 全局安装forever(进程守护模块)
+```
+npm install -g forever -save
+```
+
+## 运行
+
+- 启动服务
+```
+forever start app.js
+```
+
+- 重启服务
+```
+forever restart app.js
+```
