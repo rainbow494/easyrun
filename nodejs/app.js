b9cdcaf319740c5c238125721c00092e5114c1c7
paul.huang <paul.huang@transfinder.com>
2015-08-11 14:52:18 +0800

    修正nodejs使用手册，调用mailgun-js模块发送邮件

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,19 +2,35 @@
 var http = require('http');
 var url = require('url');
 
-var test = new Date();
+var apiKey = 'key-ea2e3ab5ee11c200168588fc18acf3a3';
+var domain = 'easyrun.hk';
+//var apiBaseUrl = 'https://api.mailgun.net/v3/easyrun.hk';
+
+var mailgun = require('mailgun-js')({apiKey: apiKey,domain:domain});
+var sendmail = function(){
+    var data = {
+      from: 'paul.huang@easyrun.hk',
+      to: 'paul.huang@easyrun.hk',
+      subject: 'Hello',
+      text: 'Testing some Mailgun awesomness!'
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+      console.log(error);
+      console.log(body);
+    });
+}
 
 var server = http.createServer(function (req,res) {
 
     //console.log(url.parse(req.url, true));
-    console.log(2);
-
     var pathname = url.parse(req.url, true).pathname;
 
     res.writeHead(200,{'Content-Type':'application/json'});
 
     switch (pathname) {
         case '/api/parsetime':
+        sendmail();
         res.write(JSON.stringify({
             "hour":  "test1"
         }));
@@ -29,5 +45,5 @@ var server = http.createServer(function (req,res) {
 
     res.end();
 })
-console.log(2);
+
 server.listen(1025);
--- nodejs/nodejs.md
+++ nodejs/nodejs.md
@@ -1,15 +1,15 @@
 ## 安装
-
 - 依赖模块安装
 ```
 npm install
 ```
+
+## 运行
+
 - 全局安装forever(进程守护模块)
 ```
 npm install -g forever -save
 ```
-
-## 运行
 
 - 启动服务
 ```
@@ -20,3 +20,22 @@ forever start app.js
 ```
 forever restart app.js
 ```
+
+## 调试
+
+- 安装调试工具
+```
+npm install -g node-inspector
+```
+
+- 运行调试（利用默认浏览器调试，浏览器必须是webkit内核）
+```
+node-debug app.js
+```
+[参考文档](http://stackoverflow.com/questions/1911015/how-to-debug-node-js-applications)
+
+## 其他可选模块
+
+- Send mail by smtp ---- **"nodemailer": "^1.4.0", **
+
+- Daemon process ---- **"forever": "^0.15.1",**
