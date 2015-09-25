fc256a027c7613d0d9ea14ad0dbb8aefd0ec4c29
paul.huang <paul.huang@transfinder.com>
2015-09-25 14:05:39 +0800

    stash code

--- nodejs/tool/input_image_batch.js
+++ nodejs/tool/input_image_batch.js
@@ -1,25 +1,69 @@
 (function () {
+    var Promise = require("../node_modules/bluebird");
+    var fs = Promise.promisifyAll(require('fs'));
+    
     var inputRoot = 'step1/category';
     var outputRoot = 'step2/category1';
-
-    var fs = require('fs');
-    var dirInit = require('./dirInit.js');
-    var dirWalker = require('./dirWalker.js');
-
-    fs.lstat(inputRoot, function (err, stats) {
-        if (err) {
-            console.log(inputRoot + ' not exist!');
-            return;
-        }
-    });
     
-    dirInit.initDirectory(outputRoot, 0);
-
-    function handleFile(imgPath, floor) {
-        var outPutImgPath = outputRoot + '/' + imgPath.slice(inputRoot.length + 1).split('/').join('-');
-        //console.info(outPutImgPath);
-        fs.link(imgPath, outPutImgPath, function () {});
+    var getFiles = function(){
+        return fs.readdirAsync(inputRoot);
     }
+    var getlstatAsync = function(filename){
+        return fs.lstatAsync(filename);
+    }
+    var linkFiles = function(filename){
+        var outPutImgPath = outputRoot + '/' + imgPath.slice(inputRoot.length + 1).split('/').join('-');
+        return fs.linkAsync(filename, outPutImgPath);
+    }
+    
+    getFiles()
+    .map(function(filename){
+        return getlstatAsync(filename);
+    })
+     .then(function(filestat){
+        return linkFiles(filename);
+    })
+    .then(function(){
+        console.log('init imgs success');
+    })
 
-    dirWalker.walk(inputRoot, 0, handleFile);
+    // var dirInit = require('./dirInit.js');
+    // var dirWalker = require('./dirWalker.js');
+
+    // function checkDirectory(path) {
+        // return fs.lstatAsync(path)
+        // .then(function (fileData) {
+            // //console.info('exist : ' + path);
+            // return;
+        // })
+        // .catch (function (error) {
+            // //console.error('\x1b[36m', 'not exist : ' + error.path, '\x1b[0m');
+            // return error.path;
+        // });
+    // }
+
+    // function handleFile(imgPath, floor) {
+        // var outPutImgPath = outputRoot + '/' + imgPath.slice(inputRoot.length + 1).split('/').join('-');
+        // fs.link(imgPath, outPutImgPath, function () {});
+    // }
+
+    // checkDirectory(inputRoot)
+        // .then(function (data) {
+            // if (data) {
+                // throw (data);
+            // }
+        // })
+        // .then(function (data) {
+            // return checkDirectory(outputRoot);
+        // })
+        // .then(function (data) {
+            // if (data) {
+                // dirInit.initDirectory(outputRoot, 0);
+            // }
+            // dirWalker.walk(inputRoot, 0, handleFile);
+        // })
+        // .catch (function (error) {
+            // console.error('\x1b[36m', 'not exist : ' + error, '\x1b[0m');
+        // })
+
 })();
