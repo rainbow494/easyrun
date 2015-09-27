cf9ed37fcfcf88a1852730c1a627ce0e6d5b8230
rainbow494 <rainbow494@gmail.com>
2015-09-27 22:21:33 +0800

    Refactored handle input image batch script

--- nodejs/tool/input_image_batch.js
+++ nodejs/tool/input_image_batch.js
@@ -1,71 +1,30 @@
-//http://stackoverflow.com/questions/32546765/iterate-directories-recursively-with-bluebird
+
 (function () {
     var Promise = require("../node_modules/bluebird");
     var fs = Promise.promisifyAll(require('fs'));
-    
+    var Path = require('path');
+
     var _inputRoot = 'step1/category';
-    var outputRoot = 'step2/category1';
-    
-    var getFiles = function(){
-        return fs.readdirAsync(_inputRoot);
-    }
-    var getlstatAsync = function(filename){
-        return fs.lstatAsync(filename).then(stat => !stat.isDirectory());
-    }
-    var linkFiles = function(filename){
-        var outPutImgPath = outputRoot + '/' + imgPath.slice(inputRoot.length + 1).split('/').join('-');
+    var _outputRoot = 'step2/category1';
+
+    var linkFiles = function (filename) {
+        var outPutImgPath = _outputRoot + '/' + filename.slice(_inputRoot.length + 1).split("\\").join('-');
         return fs.linkAsync(filename, outPutImgPath);
     }
-    
-    getFiles()
-    .map(function(item){
-        filename = _inputRoot + '/' + item;
-        return getlstatAsync(filename);
-    })
-     .then(function(filestat){
-        return linkFiles(filename);
-    })
-    .then(function(){
-        console.log('init imgs success');
-    })
 
-    // var dirInit = require('./dirInit.js');
-    // var dirWalker = require('./dirWalker.js');
+    function readDir(dirName) {
+        return fs.readdirAsync(dirName).map(function (fileName) {
+            var path = Path.join(dirName, fileName);
+            return fs.statAsync(path).then(function (stat) {
+                return stat.isDirectory() ? readDir(path) : path;
+            });
+        }).reduce(function (initialValue, filename) {
+            return linkFiles(filename);
+            //return initialValue.concat(filename);
+        }, []);
+    }
 
-    // function checkDirectory(path) {
-        // return fs.lstatAsync(path)
-        // .then(function (fileData) {
-            // //console.info('exist : ' + path);
-            // return;
-        // })
-        // .catch (function (error) {
-            // //console.error('\x1b[36m', 'not exist : ' + error.path, '\x1b[0m');
-            // return error.path;
-        // });
-    // }
-
-    // function handleFile(imgPath, floor) {
-        // var outPutImgPath = outputRoot + '/' + imgPath.slice(inputRoot.length + 1).split('/').join('-');
-        // fs.link(imgPath, outPutImgPath, function () {});
-    // }
-
-    // checkDirectory(inputRoot)
-        // .then(function (data) {
-            // if (data) {
-                // throw (data);
-            // }
-        // })
-        // .then(function (data) {
-            // return checkDirectory(outputRoot);
-        // })
-        // .then(function (data) {
-            // if (data) {
-                // dirInit.initDirectory(outputRoot, 0);
-            // }
-            // dirWalker.walk(inputRoot, 0, handleFile);
-        // })
-        // .catch (function (error) {
-            // console.error('\x1b[36m', 'not exist : ' + error, '\x1b[0m');
-        // })
-
+    readDir(_inputRoot).then(function (v) {
+        console.log("The following file has been copyed " + v.join("\n"));
+    });
 })();
