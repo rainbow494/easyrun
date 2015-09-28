add520dc8877f4e0a0672fd592e6097ec545a0e4
paul.huang <paul.huang@transfinder.com>
2015-09-28 18:12:59 +0800

    refactor code

--- nodejs/tool/input_image_batch.js
+++ nodejs/tool/input_image_batch.js
@@ -2,29 +2,49 @@
 (function () {
     var Promise = require("../node_modules/bluebird");
     var fs = Promise.promisifyAll(require('fs'));
-    var Path = require('path');
+    var path = require('path');
 
     var _inputRoot = 'step1/category';
     var _outputRoot = 'step2/category1';
 
-    var linkFiles = function (filename) {
-        var outPutImgPath = _outputRoot + '/' + filename.slice(_inputRoot.length + 1).split("\\").join('-');
-        return fs.linkAsync(filename, outPutImgPath);
-    }
+    // var linkFiles = function (filename) {
+    // var outPutImgPath = _outputRoot + '/' + filename.slice(_inputRoot.length + 1).split(path.sep).join('-');
+    // return fs.linkAsync(filename, outPutImgPath);
+    // }
 
     function readDir(dirName) {
-        return fs.readdirAsync(dirName).map(function (fileName) {
-            var path = Path.join(dirName, fileName);
-            return fs.statAsync(path).then(function (stat) {
-                return stat.isDirectory() ? readDir(path) : path;
-            });
-        }).reduce(function (initialValue, filename) {
-            return linkFiles(filename);
-            //return initialValue.concat(filename);
-        }, []);
+        return fs.readdirAsync(dirName)
+        .map(
+            function (fileName) {
+            var currentPath = path.join(dirName, fileName);
+            return fs.statAsync(currentPath)
+            .then(
+                function (stat) {
+                return stat.isDirectory() ? readDir(currentPath) : currentPath;
+            })
+        })
+        .map(
+            function (filename) {
+            //linkFiles(filename);
+            if (typeof filename === "string") {
+                var a = filename.slice(_inputRoot.length + 1);
+                console.log(a);
+                var b = a.split(path.sep);
+                var c = b.join('-');
+                var outPutImgPath = _outputRoot + '/' + c;
+                //console.log(outPutImgPath);
+                
+                //fs.linkAsync(filename, outPutImgPath);
+            }
+            return filename;
+        })
+        // .reduce(function (initialValue, filename) {
+            // //return initialValue.concat(filename);
+        // }, []);
     }
 
-    readDir(_inputRoot).then(function (v) {
-        console.log("The following file has been copyed " + v.join("\n"));
-    });
+    readDir(_inputRoot)
+    //.then(function (v) {
+        //console.log("The following file has been copyed " + v.join("\n"));
+    //});
 })();
